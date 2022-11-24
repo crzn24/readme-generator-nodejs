494a22149769a4ba0ef6c45a0706d2a14972cbc7
Nathan Cruz <113571118+crzn24@users.noreply.github.com>
2022-11-24 14:55:11 -0800

    Added array of questions to be asked in cli

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # readme-generator-nodejs
+
+licenses reference: https://gist.github.com/nicolasdao/a7adda51f2f185e8d2700e1573d8a633#3-main-types-of-open-source-licenses
--- index.js
+++ index.js
@@ -5,7 +5,53 @@ const fs = require("fs");
 const generateMarkdown = require("./utils/generateMarkdown");
 // TODO: Create an array of questions for user input
 const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is the name of your project?",
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Enter your project description:",
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "Enter your Installation Instructions:",
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Enter your Usage Information:",
+    },
+    {
+        type: "input",
+        name: "contribution",
+        message: "Enter your Contribution Guidelines:",
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Which license would you like to use?",
+        choices: ["MIT", "Apache 2.0", "GNU General Public License v3.0", "BSD-2", "BSD-3", "None"],
+    },
+    {
+        type: "input",
+        name: "username",
+        message: "What is your GitHub username?",
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your email address?",
+    },
 
+    ////////// Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+
+    // Description, Installation, Usage, Contributing, and Tests. 
+    // License.  
+    // questions section: github username, email address, 
     
 ];
 
