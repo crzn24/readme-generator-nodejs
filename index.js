53429f661a648c0c36d84f5242f811456bb5d163
Nathan Cruz <113571118+crzn24@users.noreply.github.com>
2022-11-23 21:47:40 -0800

    Opens the straight brackets in index.js to start questions array

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 const generateMarkdown = require("./utils/generateMarkdown");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+
+    
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
