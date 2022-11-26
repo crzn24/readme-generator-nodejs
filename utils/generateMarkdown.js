aa752d74abed5a67f396de4ee26f2094bedf2b53
Nathan Cruz <113571118+crzn24@users.noreply.github.com>
2022-11-25 16:44:29 -0800

    Adds if statements in functions for rendering badges and links for all licenses

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,12 +2,63 @@
 // If there is no license, return an empty string
 // "MIT", "Apache 2.0", "GNU General Public License v3.0", "ISC License", "BSD-2", "BSD-3", "None"
 function renderLicenseBadge(license) {
-  if (license === "None")
+  if (license === "MIT") {
+    return `![${license}](https://img.shields.io/badge/License-MIT-brightgreen)`
+  }
+  if (license === "Apache 2.0") {
+    return `![${license}](https://img.shields.io/badge/License-Apache_2.0-brightgreen)`
+  }
+  if (license === "GNU General Public License v3.0") {
+    return `![${license}](https://img.shields.io/badge/License-GPLv3-brightgreen)`
+  }
+  if (license === "ISC License") {
+    return `![${license}](https://img.shields.io/badge/License-ISC-brightgreen)`
+  }
+  if (license === "BSD-2") {
+    return `![${license}](https://img.shields.io/badge/License-BSD_2--Clause-brightgreen)`
+  }
+  if (license === "BSD-3") {
+    return `![${license}](https://img.shields.io/badge/License-BSD_3--Clause-brightgreen)`
+  }
+  
+  if (license === "None") {
+    return ``;
+  }
+  else {
+    return ``;
+  }
+
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
+  if (license === "MIT") {
+    return `https://opensource.org/licenses/MIT`
+  }
+  if (license === "Apache 2.0") {
+    return `https://opensource.org/licenses/Apache-2.0`
+  }
+  if (license === "GNU General Public License v3.0") {
+    return `https://www.gnu.org/licenses/gpl-3.0`
+  }
+  if (license === "ISC License") {
+    return `https://opensource.org/licenses/ISC`
+  }
+  if (license === "BSD-2") {
+    return `https://opensource.org/licenses/BSD-2-Clause`
+  }
+  if (license === "BSD-3") {
+    return `https://opensource.org/licenses/BSD-3-Clause`
+  }
+
+  if (license === "None") {
+    return ``;
+  }
+  else {
+    return ``;
+  }
+
 
 }
 
