d0a149cef0c8da5d7b46fd6b1a93d7a0fbf8f47a
Nathan Cruz <113571118+crzn24@users.noreply.github.com>
2022-11-25 17:26:43 -0800

    Attempts function to render license section

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -65,7 +65,7 @@ function renderLicenseLink(license) {
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-
+  return `This project created by ${username}, uses: [${license}](${renderLicenseLink(license)})`
 }
 
 // TODO: Create a function to generate markdown for README
@@ -74,30 +74,41 @@ function generateMarkdown(data) {
 
   ${renderLicenseBadge(data.license)}
 
-  ## Description
-  ${data.description}
   ## Table of Contents
-
+  - [Description](#description)
   - [Installation](#installation)
   - [Usage](#usage)
-  - [Credits](#credits)
   - [License](#license)
+  - [How to Contribute](#how to contribute)
+  - [Tests](#tests)
+
+
+  ## Description
+  ${data.description}
 
   ## Installation
+
   ${data.installation}
+
   ## Usage
+
   ${data.usage}
+
   ## License
+
   ${data.}
 
   ---
 
   ## How to Contribute
+
   ${data.contribution}
+
   Contact me: ${data.email}
   Find me on GitHub: https://github.com/${data.username}
 
   ## Tests
+
   ${data.tests}
 
 `;
